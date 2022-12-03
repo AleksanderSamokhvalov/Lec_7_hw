47afe34794422bd52d7c609dbfcfffa8462a5235
Mr.Poxy/\b <solom.solom2910@gmail.com>
2022-12-03 14:52:13 +0300

    Task_50_done

--- hw_task_50/Program.cs
+++ hw_task_50/Program.cs
@@ -1,2 +1,49 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задача 50. Напишите программу, которая на вход принимает позиции элемента в двумерном массиве, и возвращает значение этого элемента или же указание, что такого элемента нет.
+
+// Например, задан массив:
+
+// 1 4 7 2
+
+// 5 9 2 3
+
+// 8 4 2 4
+
+// 17 -> такого числа в массиве нет
+
+
+Console.WriteLine("Введите количество строк");
+int rows = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Введите количество столбцов");
+int columns = Convert.ToInt32(Console.ReadLine());
+
+int element = 0;
+
+int [,] matrix = new int [rows,columns];
+
+for (int i = 0; i < matrix.GetLength(0); i++)
+{
+    for (int j = 0; j < matrix.GetLength(1); j++)
+    {
+        matrix[i,j] = new Random().Next(0,10);
+        Console.Write(matrix[i,j] +   " ");
+    }
+    Console.WriteLine("");
+}
+Console.WriteLine("=========================");
+Console.WriteLine("Задайте позицию элемента по i");
+int num_i = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Задайте позицию элемента по j");
+int num_j = Convert.ToInt32(Console.ReadLine());
+
+if (num_i<=rows && num_j<=columns)
+    {
+    element = matrix[num_i,num_j];
+    Console.WriteLine("Значение заданного элемента = " + element);
+    }
+else
+{
+Console.WriteLine("Такого элемента нет");    
+}
+
--- hw_task_52/Program.cs
+++ hw_task_52/Program.cs
@@ -1,2 +1,35 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задача 52. Задайте двумерный массив из целых чисел. Найдите среднее арифметическое элементов в каждом столбце.
+
+// Например, задан массив:
+// 1 4 7 2
+// 5 9 2 3
+// 8 4 2 4
+// Среднее арифметическое каждого столбца: 4,6; 5,6; 3,6; 3.
+
+
+Console.WriteLine("Введите количество строк");
+int rows = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Введите количество столбцов");
+int columns = Convert.ToInt32(Console.ReadLine());
+
+int [] average = new int [columns];
+
+int [,] matrix = new int [rows,columns];
+
+for (int i = 0; i < matrix.GetLength(0); i++)
+{
+    for (int j = 0; j < matrix.GetLength(1); j++)
+    {
+        matrix[i,j] = new Random().Next(0,10);
+        Console.Write(matrix[i,j] +   " ");
+    }
+    Console.WriteLine();
+}
+
+for (int j = 0; j < matrix.GetLength(1); j++)
+{
+    average=5/rows;
+    Console.Write(average + " ");
+}
+// Console.WriteLine("Сумма элементов главной диагонали " + summ);
